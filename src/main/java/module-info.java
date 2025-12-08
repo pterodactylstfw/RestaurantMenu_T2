5e997c1dfe5933f7faeb3b92bda640dacd2aec49
Raul-Nicolae Constantin <constantin.raul10@gmail.com>
2025-12-08 20:24:06 +0200

    Add Hibernate and PostgreSQL dependencies; configure persistence settings

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,9 +2,15 @@ module unitbv.mip {
     requires javafx.controls;
     requires javafx.fxml;
     requires com.fasterxml.jackson.databind;
+    requires org.hibernate.orm.core;
+    requires jakarta.persistence;
+
+    uses jakarta.persistence.spi.PersistenceProvider;
 
     opens unitbv.mip to javafx.graphics, javafx.fxml;
-    opens unitbv.mip.model to javafx.base;
+    opens unitbv.mip.model to javafx.base, org.hibernate.orm.core,
+            com.fasterxml.jackson.databind;
+
 
     exports unitbv.mip;
 }
